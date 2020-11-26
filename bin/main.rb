3812223316cadddd6f743d65f81f40fe879cce71
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-11-26 15:17:59 +0200

    added a constant with winning numbers

--- bin/main.rb
+++ bin/main.rb
@@ -2,21 +2,18 @@
 class TicTacToe
   attr_accessor :board
 
-  puts "Welcome, Input a number from 1-9"
-  $user_input = gets.chomp
-
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
- end
-        def user_input(num)
-            num = [1, 2, 3, 4 ,5, 6, 7,8, 9]
-            if $user_input == num.each
-                print "your move has been made"
-            elsif $user_input == !num.each
-                puts "error"
-            end
-    end
-
+	end
+	
+	# Winning numbers
+	WINNING NUMBERS = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7]]
+# getting user input
+	def user_input
+		puts "Choose a number between 1 to 9 to the play game"
+		inputs = gets.chomp
+		inputs.to_i - 1
+	end
 
 	def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -24,9 +21,17 @@ class TicTacToe
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts " ----------- "
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+	end
+
+	def user_input_to_doard(num)
+
+		if num	== (1..9) 
+			@board[0] = num
+		end
+		return @board
+	end
+	
 end
 
 game = TicTacToe.new
 game.display_board
-game.user_input(3)
