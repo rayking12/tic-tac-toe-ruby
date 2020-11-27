fa628a7f74b07d75dad5b9dd3b872171489949ef
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-11-27 12:20:18 +0200

    added win method

--- bin/main.rb
+++ bin/main.rb
@@ -84,27 +84,37 @@ class TicTacToe
     players
 	end
 	
-	def win? 
-		WIN_COMBINATIONS.detect do |win_combination| 
-			if win_combination[0] == @board[0] && win_combination[1] == @board[0]
-				return true
-	end
+  def win?
+    WIN_COMBINATIONS.each_with_index do |elem, child_arr|
+      @board[child_arr[0]] == @board[child_arr[1]] &&
+      @board[child_arr[1]] == @board[child_arr[2]] &&
+      position_taken?(child_arr[0])
+    end
+  end
+
+	def game_full?(board)
+		board.all? {|i| i == "X" || i == "O"}
+ end
 
   def game_on?
-    turn_counts == 9 || !win? ? true : game_play?
+    turn_counts < 9 && !win? ? true : false
 	end
 	
 	def draw?
-		!win? && (@board == 'X' || @board == 'O') 
-	end
+	!win? && game_full?
+ end
 
-  def game_over?
-		win? || draw?
-	end
+	def game_over?
+		if draw? || win? || game_full?
+			return true
+		end
+		board
+ end
 
   def game_play
     user_input until game_over?
-  end
+	end
+
 end
 game = TicTacToe.new
 game.name
