19d71f6fe8344a6e3e669d4cc4eeeda44b8b180e
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-11-30 12:10:47 +0200

    added game_logic class

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,9 +1,21 @@
 class GameLogic
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [6, 4, 2],
+    [0, 4, 8]
+  ].freeze
+
   def win?
     WIN_COMBINATIONS.detect do |child_arr|
       @board[child_arr[0]] == @board[child_arr[1]] && @board[child_arr[1]] == @board[child_arr[2]] &&
         position_taken?(child_arr[0])
     end
+    child_arr
   end
 
   def game_full?
@@ -25,10 +37,16 @@ class GameLogic
   def name
     puts 'Player1: what is Your name?'
     @name = gets.chomp
-    puts 'Welcome ' + @name
+    if @name.to_s? 
+       puts 'Welcome ' + @name 
+       else
+        return gets.chomp
     puts 'Player2: What is your name?'
     @name_two = gets.strip
+    if @name_two.to_s?
     puts 'Player2 : Welcome ' + @name_two
+    else
+      return gets.chomp
   end
 
   def winner
