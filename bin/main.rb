8b50cd0da84de58f67734933d46e586bd8e848ea
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-12-01 11:36:55 +0200

    added validations for input in player names

--- bin/main.rb
+++ bin/main.rb
@@ -1,89 +1,29 @@
-#!/usr/bin/env ruby
-module WinNumbers
-  WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [6, 4, 2],
-    [0, 4, 8]
-  ].freeze
-end
-class TicTacToe
-  include WinNumbers
-  def initialize
-    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-  end
-
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ----------- '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ----------- '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-  def user_input_to_doard(arr)
-    @board[0] = arr if arr == (1..9)
-    @board
-  end
-
-  def input_to_index(input)
-    input.to_i - 1
-  end
-
-  def move(position, token = 'X')
-    @board[position] = token
-  end
-
-  def position_taken?(input)
-    @board[input] == 'X' || @board[input] == 'O'
-  end
-
-  def current_player
-    @players = turn_counts.even? ? 'X' : 'O'
-    @players
-  end
-
-  def player_name1
-    puts 'you are the first player, Enter your name'
-    @player = names
-    puts 'welcome' + @player
-  end
-
-  def win?
-    WIN_COMBINATIONS.detect do |child_arr|
-      @board[child_arr[0]] == @board[child_arr[1]] && @board[child_arr[1]] == @board[child_arr[2]] &&
-        position_taken?(child_arr[0])
-    end
-  end
-
-  def game_full?
-    board.all? { |i| %w[O X].include?(i) }
-  end
-
-  # getting user input
-  def user_input
-    puts 'Choose a number between 1 to 9 to play game'
-    inputs = gets.strip
-    inputs = input_to_index(inputs)
-    if valid_move?(inputs)
-      move(inputs, current_player)
-    elsif !valid_move?(inputs)
-      puts 'Invalid move! Make a valid move from the unoccupied position'
+require_relative '../lib/game_logic'
+class Play
+  def names
+    @name = gets.chomp
+    case @name
+    when /\D/
+      puts 'Welcome ' + @name
     else
-      self
+      puts 'Please do not enter numbers or symbols'
+      return self unless @name =~ /^-?[0-9]+$/
     end
-    display_board
   end
 
-  def turn_counts
-    taken_spot = 0
-    @board.each do |i|
-      taken_spot += 1 if %w[X O].include?(i)
-    end
-    taken_spot
+  def player1
+    puts 'Hi,Welcome first player, please enter your name'
+    names
+  end
+
+  def player2
+    puts 'Hi, Welcome second player, please enter your name'
+    names
   end
 end
+
+plays = Play.new
+plays.player1
+plays.player2
+game = TicTacToe.new
+game.game_play
--- bin/play.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/game_logic'
-require_relative './bin/main'
-class Play
-	game = GameLogic.new
-	case game.names
-	when game.@current_player == game.@Players
-	puts "#{game.@current_player} enter your name!"
-
-end
