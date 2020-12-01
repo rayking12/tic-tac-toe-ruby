07c4ec4c9e732056ff90612482c4b89c124dd331
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-12-01 10:05:38 +0200

    removed a module in game_logic file

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,19 +1,14 @@
-module WinNumbers
-  WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [6, 4, 2],
-    [0, 4, 8]
-  ].freeze
-end
+require_relative '../bin/main'
 class GameLogic
-  attr_accessor :board
-  include WinNumbers
-  require_relative '../bin/main'
+  class Game
+    attr_reader :players, :board, :current_player, :other_player
+    def initialize(players, board = Board.new)
+      @players = players
+      @board = board
+      @current_player, @other_player = players.shuffle
+    end
+  end
+
   def valid_move?(input)
     input.between?(0, 9) && !game.position_taken?(input)
   end
@@ -30,8 +25,7 @@ class GameLogic
     draw? || win?
   end
 
-  def name
-    puts 'Player1: what is Your name?'
+  def names
     @name = gets.chomp
     case @name
     when /\D/
@@ -41,17 +35,6 @@ class GameLogic
       gets.chomp
     end
     name while @name =~ /^-?[0-9]+$/
-
-    puts 'Player2: what is Your name?'
-    @name_two = gets.chomp
-    case @name_two
-    when /\D/
-      puts 'Welcome ' + @name_two
-    else
-      puts 'Please put in a valid input'
-      gets.chomp
-    end
-    name while @name_two =~ /^-?[0-9]+$/
   end
 
   def winner
@@ -70,5 +53,3 @@ class GameLogic
   end
 end
 
-game = TicTacToe.new
-game.game_play
