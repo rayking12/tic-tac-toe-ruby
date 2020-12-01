d4a893c0c19510b2d54e4427a23a0a60db140fc3
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-12-01 11:33:28 +0200

    added validations to player naming

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,20 +1,23 @@
-require_relative '../bin/main'
+#!/usr/bin/env ruby
+module WinNumbers
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [6, 4, 2],
+    [0, 4, 8]
+  ].freeze
+end
 class GameLogic
-  class Game
-    attr_reader :players, :board, :current_player, :other_player
-    def initialize(players, board = Board.new)
-      @players = players
-      @board = board
-      @current_player, @other_player = players.shuffle
-    end
-  end
-
   def valid_move?(input)
-    input.between?(0, 9) && !game.position_taken?(input)
+    input.between?(0, 9) && !position_taken?(input)
   end
 
   def game_on?
-    game.turn_counts < 9 && !win? ? true : false
+    turn_counts < 9 && !win? ? true : false
   end
 
   def draw?
@@ -23,18 +26,6 @@ class GameLogic
 
   def game_over?
     draw? || win?
-  end
-
-  def names
-    @name = gets.chomp
-    case @name
-    when /\D/
-      puts 'Welcome ' + @name
-    else
-      puts 'Please put in a valid input'
-      gets.chomp
-    end
-    name while @name =~ /^-?[0-9]+$/
   end
 
   def winner
@@ -52,4 +43,80 @@ class GameLogic
     end
   end
 end
+class TicTacToe < GameLogic
+  attr_accessor :board
+  include WinNumbers
+  def initialize
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  end
 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts ' ----------- '
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts ' ----------- '
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def user_input_to_doard(arr)
+    @board[0] = arr if arr == (1..9)
+    @board
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(position, token = 'X')
+    @board[position] = token
+  end
+
+  def position_taken?(input)
+    @board[input] == 'X' || @board[input] == 'O'
+  end
+
+  def current_player
+    @players = turn_counts.even? ? 'X' : 'O'
+    @players
+  end
+
+  def player_name1
+    puts 'you are the first player, Enter your name'
+    @player = names
+    puts 'welcome' + @player
+  end
+
+  def win?
+    WIN_COMBINATIONS.detect do |child_arr|
+      @board[child_arr[0]] == @board[child_arr[1]] && @board[child_arr[1]] == @board[child_arr[2]] &&
+        position_taken?(child_arr[0])
+    end
+  end
+
+  def game_full?
+    board.all? { |i| %w[O X].include?(i) }
+  end
+
+  # getting user input
+  def user_input
+    puts 'Choose a number between 1 to 9 to play game'
+    inputs = gets.strip
+    inputs = input_to_index(inputs)
+    if valid_move?(inputs)
+      move(inputs, current_player)
+    elsif !valid_move?(inputs)
+      puts 'Invalid move! Make a valid move from the unoccupied position'
+    else
+      self
+    end
+    display_board
+  end
+
+  def turn_counts
+    taken_spot = 0
+    @board.each do |i|
+      taken_spot += 1 if %w[X O].include?(i)
+    end
+    taken_spot
+  end
+end
