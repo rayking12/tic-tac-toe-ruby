b41cf92746a536d3ec3257208d1de797c68bf48c
unknown <rayodo667@gmail.com>
2020-12-02 07:36:40 +0100

    fixed player2 method

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-require_relative '../lib/game_logic'
+require_relative '../lib/game_logic.rb'
 class Play
   def names
     @name = gets.chomp
@@ -22,32 +22,34 @@ class Play
     puts 'Hi, Welcome second player, please enter your name'
     names
     player2 while @name =~ /^-?[0-9]+$/
+  end
 
-    def game_play
-      user_input until TicTacToe.new.game_over?
-      if TicTacToe.new.win?
-        winner = winner()
-        puts "Congratulations #{@name}!"
-      elsif draw?
-        puts 'Good try, You draw!'
-      end
-    end
+  my_game = GameLogic.new
 
-    # getting user input
-    def user_input
-      puts 'Choose a number between 1 to 9 to play game'
-      inputs = gets.strip
-      inputs = TicTacToe.new.input_to_index(inputs)
-      if TicTacToe.new.valid_move?(inputs)
-        TicTacToe.new.move(inputs, TicTacToe.new.current_player)
-      elsif !TicTacToe.new.valid_move?(inputs)
-        puts 'Invalid move! Make a valid move from the unoccupied position'
-      else
-        self
-      end
-      TicTacToe.new.display_board
+  def game_play
+    user_input until TicTacToe.new.game_over?
+    if TicTacToe.new.win?
+      winner = winner()
+      puts "Congratulations #{@name}!"
+    elsif draw?
+      puts 'Good try, You draw!'
     end
-end
+  end
+
+  # getting user input
+  def user_input
+    puts 'Choose a number between 1 to 9 to play game'
+    inputs = gets.strip
+    inputs = TicTacToe.new.input_to_index(inputs)
+    if TicTacToe.new.valid_move?(inputs)
+      TicTacToe.new.move(inputs, TicTacToe.new.current_player)
+    elsif !TicTacToe.new.valid_move?(inputs)
+      puts 'Invalid move! Make a valid move from the unoccupied position'
+    else
+      self
+    end
+    TicTacToe.new.display_board
+  end
 end
 
 plays = Play.new
